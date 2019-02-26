065b7512d5f2481d1969bb1447d031f379502fe4
Randy <rlthomasccv@gmail.com>
2019-02-26 01:31:08 -0800

    Refactor - REFRESH button needs fix

--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable no-var */
@@ -10,7 +11,7 @@ class Output extends Component {
     super(props);
   }
 
-  output() {
+  movieData() {
     const { data } = this.props;
     var flatData = [];
     for (let i = 0; i < data.length; i++) {
@@ -33,6 +34,15 @@ class Output extends Component {
         <div>
           <img src={`https://image.tmdb.org/t/p/w300/${randomMovieThree.poster_path}`} />
         </div>
+      </div>
+    );
+  }
+
+  output() {
+    return (
+      <div>
+        {this.movieData()}
+        <button type="button" className="btn btn-success btn-lg" onClick={this.movieData}>Refresh</button>
       </div>
     );
   }
